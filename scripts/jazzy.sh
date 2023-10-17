eceda7a2e60f7bba4f19c8bb5632c26e626e77a5
William Towe <willbur1984@gmail.com>
2023-10-16 20:07:03 -0400

    Add update_docs lane

---
+++ scripts/jazzy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+MODULE=$1
+INFO_PLIST=$2
+
+if [ -z $MODULE ]
+then
+    echo "First argument (module name) is empty"
+    exit 1
+fi
+
+if [ -z $INFO_PLIST ]
+then
+    echo "Second argument (info plist path) is empty"
+    exit 1
+fi
+
+bundle exec jazzy --clean --author Kosoku --author_url http://kosoku.com/ --source-host github --source-host-url https://github.com/Kosoku/$MODULE --module-version $(./Overseer/scripts/print_info_plist_key.sh "CFBundleShortVersionString" $INFO_PLIST) --build-tool-arguments -scheme,Feige --module $MODULE --root-url https://kosoku.github.io/$MODULE/ --output docs --theme fullwidth
